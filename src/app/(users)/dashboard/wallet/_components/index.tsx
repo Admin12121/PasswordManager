2ee0ee6b8b5a5cdc03232e4d28969821223d8820
admin12121 <vickytaj6459@gmail.com>
2025-03-11 00:05:34 +0545

    type error fixed

--- src/app/(users)/dashboard/wallet/_components/index.tsx
+++ src/app/(users)/dashboard/wallet/_components/index.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import CardPage from "./card";
-import AccountPage from "./account";
 
 const Wallet = () => {
   return (
     <div className="flex flex-col gap-4">
-      <CardPage />
-      <AccountPage />
     </div>
   );
 };
